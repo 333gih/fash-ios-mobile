0423638da3208832c8435b61a5bbaa478b1b0d59
MinhPhuc <mphuc8671@gmail.com>
2026-05-26 12:14:07 +0700

    fix-build-after-fix-13456

--- Fash/data/editorial/EditorialGuideRepository.swift
+++ Fash/data/editorial/EditorialGuideRepository.swift
@@ -2,9 +2,5 @@ import Foundation
 
 /// Port of Android `EditorialGuideRepository` (data.editorial).
 final class EditorialGuideRepository {
-    private let deps: AppDependencies
-
-    init(deps: AppDependencies) {
-        self.deps = deps
-    }
+    init() {}
 }
