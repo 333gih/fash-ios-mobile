bd84106762ee08f899a39a81fca9388c357122ec
MinhPhuc <mphuc8671@gmail.com>
2026-05-27 22:12:10 +0700

    Fix iPad orientation requirements for TestFlight upload

--- scripts/sync_app_icon_from_android.ps1
+++ scripts/sync_app_icon_from_android.ps1
@@ -14,23 +14,23 @@ if (-not (Test-Path $srcPath)) {
 }
 
 Add-Type -AssemblyName System.Drawing
-# Matches Android ic_launcher_background (#F04170)
+# Matches Android ic_launcher_background (#F04170). App Store requires opaque RGB (no alpha).
 $bgColor = [System.Drawing.Color]::FromArgb(255, 0xF0, 0x41, 0x70)
 $src = [System.Drawing.Image]::FromFile($srcPath)
-$canvas = New-Object System.Drawing.Bitmap 1024, 1024
+$srcW = $src.Width
+$srcH = $src.Height
+$canvas = New-Object System.Drawing.Bitmap 1024, 1024, ([System.Drawing.Imaging.PixelFormat]::Format24bppRgb)
 $g = [System.Drawing.Graphics]::FromImage($canvas)
 $g.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
 $g.PixelOffsetMode = [System.Drawing.Drawing2D.PixelOffsetMode]::HighQuality
 $g.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::HighQuality
-$g.FillRectangle((New-Object System.Drawing.SolidBrush $bgColor), 0, 0, 1024, 1024)
-$scale = [Math]::Min(1024.0 / $src.Width, 1024.0 / $src.Height)
-$newW = [int][Math]::Round($src.Width * $scale)
-$newH = [int][Math]::Round($src.Height * $scale)
+$g.Clear($bgColor)
+$scale = [Math]::Min(1024.0 / $srcW, 1024.0 / $srcH)
+$newW = [int][Math]::Round($srcW * $scale)
+$newH = [int][Math]::Round($srcH * $scale)
 $x = [int][Math]::Round((1024 - $newW) / 2.0)
 $y = [int][Math]::Round((1024 - $newH) / 2.0)
-$srcW = $src.Width
-$srcH = $src.Height
 $g.DrawImage($src, $x, $y, $newW, $newH)
 $canvas.Save($dstPath, [System.Drawing.Imaging.ImageFormat]::Png)
 $g.Dispose(); $canvas.Dispose(); $src.Dispose()
-Write-Host "Wrote $dstPath (1024x1024) from $srcPath (${srcW}x${srcH})"
+Write-Host "Wrote $dstPath (1024x1024 RGB, no alpha) from $srcPath (${srcW}x${srcH})"
